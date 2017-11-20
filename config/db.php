5d2cfee3442af99bf33a7762beaa3d515cd4b3e6
Mikhail <mixartemev@gmail.com>
2017-11-20 19:08:23 +0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=server64.hosting.reg.ru;dbname=u6612657_cointb2',
-    'username' => 'u6612657_cointb2',
-    'password' => 'QjOuqAoW',
-    'charset' => 'utf8',
-];
