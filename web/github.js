0f0679fb5ea00b50ad1cbb6271be90973921f17c
mixartemev <mixartemev@gmail.com>
2017-11-21 08:59:40 +0300

    phantom

--- web/github.js
+++ web/github.js
@@ -1,5 +1,5 @@
 var page = require('webpage').create();
-page.open('http://bitparse/index.php?r=site&var=1', function() {
+page.open('http://coin.tbcgroupm.ru/bitparse/web/?var=1', function() {
     page.render('github.png');
     phantom.exit();
 });
