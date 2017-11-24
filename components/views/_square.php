31740aa4fb72fd720e08108cba4f594a57187f73
mixartemev <mixartemev@gmail.com>
2017-11-24 18:17:50 +0300

    4

--- components/views/_square.php
+++ components/views/_square.php
@@ -9,15 +9,17 @@
 use app\models\History;
 $prd = ['24h' => 'day', '1h' => 'hour', '7d' => 'week']
 ?>
-<div class="square">
-	<p class="date"><?= date('d M Y')?><br><span class="cap-sum"><?= $prd[$period] ?></span></p>
-	<table class="coins-list">
-	<?php
-	$cap_summ = 0;
-	$cap_charge_summ = 0;
-	//var_dump($model);die;
-	foreach(History::$coins as $k => $coin){
-	    //if($k!=2){
+<div class="wrap b-map">
+    <div class="b-map__year text-center"><?= date('Y')?></div>
+    <div class="b-map__month text-center"><?= date('F d')?> day</div>
+
+    <table class="b-map__table">
+	    <?php
+	    $cap_summ = 0;
+	    $cap_charge_summ = 0;
+	    //var_dump($model);die;
+	    foreach(History::$coins as $k => $coin){
+		    //if($k!=2){
 		    if($type=='cap'){
 			    $cap_summ += $model[$k][$type];
 			    $charge = $cap_charge_24h[$k];
@@ -27,32 +29,58 @@ $prd = ['24h' => 'day', '1h' => 'hour', '7d' => 'week']
 		    }
 		    $cap_summ += $model[$k][$type]; // на случай если это капитализация
 		    ?>
+            <tr data-coin="<?= $model[$k]['name'] ?>">
+                <td class="b-map__table-first">
+                    <img src="/img/<?= $model[$k]['name'] ?>.png">
+                    <span class="b-map__name-coin"><?= $coin[$model[$k]['name']] ?></span>
+                </td>
+                <td class="b-map__table-last">
+                    <span class="b-map__num-coin"><?= History::$currencies[$cur] . Yii::$app->formatter->asDecimal($model[$k][$type]/*, $type=='cap'?0:2*/) ?></span>
+                    <span class="b-map__label <?= $charge > 0 ? 'green' : 'red' ?> b-map__diff-coin"><?= Yii::$app->formatter->asDecimal($charge) ?>% / 54742.43</span>
+                </td>
+            </tr>
+
             <tr class="coin-row">
                 <td class="coin-name">
                     <img src="img/<?= $model[$k]['name'] ?>.png"> &nbsp;
 				    <?= $coin[$model[$k]['name']] ?>
                 </td>
                 <td class="coin-value">
-                    <span class="price"><?= History::$currencies[$cur] . Yii::$app->formatter->asDecimal($model[$k][$type]/*, $type=='cap'?0:2*/) ?></span>
-                    <span class="charge <?= $charge > 0 ? 'up' : 'down' ?>">(<?= Yii::$app->formatter->asDecimal($charge) ?>%)</span>
+                    <span class="price"></span>
+                    <span class="charge ">(%)</span>
                 </td>
             </tr>
 		    <?php
-	    //}
-} ?>
-	</table>
-	<div class="cap">
-<?php if($type == 'cap'){ ?>
-		<b>Market Cap All Coins:</b> <?= History::$currencies[$cur] . Yii::$app->formatter->asDecimal($cap_summ, 0) ?> / <span class="cap-sum"><?= Yii::$app->formatter->asDecimal($cap_charge_summ) ?>%</span>
-<?php } ?>
-	</div>
-	<div class="courses">
-<?php
-if($withCourses){
-	foreach($courses as $k => $course){
-		echo strtoupper($k) .' '. Yii::$app->formatter->asDecimal($course, 4) . " &nbsp; &nbsp; ";
-	}
-}
- ?>
-	</div>
+		    //}
+	    } ?>
+    </table>
+
+    <!--div class="cap">
+		<?php if($type == 'cap'){ ?>
+            <b>Market Cap All Coins:</b> <?= History::$currencies[$cur] . Yii::$app->formatter->asDecimal($cap_summ, 0) ?> / <span class="cap-sum"><?= Yii::$app->formatter->asDecimal($cap_charge_summ) ?>%</span>
+		<?php } ?>
+	</div-->
+
+    <div class="b-map__charts">
+        <table class="table-ch">
+            <tr class="table-ch__currency-name">
+	            <?php if($withCourses){
+		            foreach($courses as $k => $course){ ?>
+                        <td><?= strtoupper($k) ?></td>
+		            <?php }
+	            } ?>
+                <td>GBR</td>
+                <td>EUR</td>
+                <td>USD</td>
+                <td>JPY</td>
+            </tr>
+            <tr class="table-ch__currency-num">
+	            <?php if($withCourses){
+		            foreach($courses as $k => $course){ ?>
+                        <td><?= Yii::$app->formatter->asDecimal($course, 4) ?></td>
+		            <?php }
+	            } ?>
+            </tr>
+        </table>
+    </div>
 </div>
