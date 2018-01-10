4080f7ab6771e63038d7bad0ec0a7a9cb82603a5
mix <mixartemev@gmail.com>
2018-01-10 10:40:45 +0300

    rub

--- components/views/_square.php
+++ components/views/_square.php
@@ -35,7 +35,7 @@ $prd = ['24h' => 'day', '1h' => 'hour', '7d' => 'week']
                     <span class="b-map__name-coin"><?= $coin[$model[$k]['name']] ?></span>
                 </td>
                 <td class="b-map__table-last">
-                    <span class="b-map__num-coin"><span class="b-rub"><?= History::$currencies[$cur] ?></span> <?= Yii::$app->formatter->asDecimal($model[$k][$type]/*, $type=='cap'?0:2*/) ?></span>
+                    <span class="b-map__num-coin"><span class="b-rub"><?= History::$currencies[$cur] ?></span><?= Yii::$app->formatter->asDecimal($model[$k][$type]/*, $type=='cap'?0:2*/) ?></span>
                     <span class="b-map__label <?= $charge > 0 ? 'green' : 'red' ?> b-map__diff-coin"><?= Yii::$app->formatter->asDecimal($charge) ?>% / 54742.43</span>
                 </td>
             </tr>
