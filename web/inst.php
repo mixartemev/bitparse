2a7cdb4139de485b7f695634c29865c610c1e6bd
mix <mixartemev@gmail.com>
2018-01-14 14:27:22 +0300

    inst date

--- web/inst.php
+++ web/inst.php
@@ -2,4 +2,4 @@
 require_once 'Instagram.php';
 $i = new Instagram('tmtsignal','Lenadima137513selyT', true);
 //$i->login();
-$i->uploadPhoto('coin.jpg', 'CryptoRate');
+$i->uploadPhoto('coin.jpg', 'CryptoRate - ' . date('d.m.Y'));
