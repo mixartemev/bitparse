5238d531f9608908eb23e164ccf81a3c0285405a
mix <mixartemev@gmail.com>
2018-01-24 21:26:30 +0300

    hashtag

--- web/inst.php
+++ web/inst.php
@@ -2,4 +2,7 @@
 require_once 'Instagram.php';
 $i = new Instagram('tmtsignal','Lenadima137513selyT', true);
 //$i->login();
-$i->uploadPhoto('coin.jpg', 'CryptoRate - ' . date('d.m.Y'));
+$i->uploadPhoto('coin.jpg', 'CryptoRate - ' . date('d.m.Y'). "
+\r\n\r\n\r\n
+#bitcoin #crypto #ico #maining #биткоин #криптовалюта #блокчейн #invest #money #coin #blockchain #cryptocurrency
+") ;
